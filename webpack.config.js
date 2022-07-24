ae3ce65d3e0218fa991ff55e61c9ac427f9c9e82
reynoldnjord <njordreynold@gmail.com>
2022-07-24 16:59:40 +0300

    Used prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
-const path = require('path');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        about: './src/about.js',
-        home: './src/home.js',
-        menu: '/src/menu.js',
-        website: '/src/website.js',
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            title: 'Restaurant',
-        }),
+  entry: {
+    index: "./src/index.js",
+    about: "./src/about.js",
+    home: "./src/home.js",
+    menu: "/src/menu.js",
+    website: "/src/website.js",
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      title: "Restaurant",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
+  },
 };
